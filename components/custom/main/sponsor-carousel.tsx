802d24af9690336b5064806d5335c02831433445
Amir Husayn Panahifar <ahp@panahifar.ir>
2025-11-26 00:08:59 +0330

    Add functional view of sponsor section

--- components/custom/main/sponsor-carousel.tsx
+++ components/custom/main/sponsor-carousel.tsx
@@ -4,9 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 
 export default function SponsorsCarousel() {
-  if (!sponsors || sponsors.length === 0) {
-    return null;
-  }
+  if (!sponsors || sponsors.length === 0) return null;
 
   return (
     <div className="container max-w-7xl mx-auto py-14 px-4">
@@ -14,15 +12,16 @@ export default function SponsorsCarousel() {
         <h2 className="text-2xl sm:text-3xl font-bold">Our Sponsors</h2>
       </div>
 
-      <div className="grid grid-cols-3 gap-8">
+      <div className="flex flex-wrap justify-center gap-8">
         {sponsors.map((sponsor) => (
-          <Card className="md:col-span-1 col-span-3" key={sponsor.id}>
-            <CardContent className="flex justify-center">
+          <Card key={sponsor.id} className="p-4 flex flex-col items-center">
+            <CardContent className="flex justify-center items-center h-28">
               <Image
                 src={sponsor.logo}
                 alt={sponsor.name}
                 width={80}
                 height={80}
+                className="object-contain"
               />
             </CardContent>
             <h3 className="font-semibold text-lg text-center">
@@ -33,12 +32,14 @@ export default function SponsorsCarousel() {
       </div>
 
       <div className="flex justify-center mt-12">
-        <Button
-          size="lg"
-          className="text-lg px-8 bg-parch p-6 rounded-full ring-secondary"
-        >
-          Become our sponsor
-        </Button>
+        <a href="mailto:community@parchlinux.com">
+          <Button
+            size="lg"
+            className="text-lg flex flex-col items-center px-8 bg-parch p-6 rounded-full ring-secondary"
+          >
+            <span>Become our sponsor</span>
+          </Button>
+        </a>
       </div>
     </div>
   );
